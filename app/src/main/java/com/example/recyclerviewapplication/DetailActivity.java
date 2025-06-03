98c6915ef86ab795f3b1137234a1372f2e5003f6
LENOVO <nguyendiem17042003@gmail.com>
2025-06-03 17:00:13 +0700

    Initial commit

--- app/src/main/java/com/example/recyclerviewapplication/DetailActivity.java
+++ app/src/main/java/com/example/recyclerviewapplication/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.recyclerviewapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -34,6 +37,18 @@ public class DetailActivity extends AppCompatActivity {
 
         btnAddToCart.setOnClickListener(v -> {
             Toast.makeText(this, "Added to cart: " + product.name, Toast.LENGTH_SHORT).show();
+        });
+
+        TextView tvStorePhone = findViewById(R.id.tvStorePhone);
+
+        tvStorePhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = tvStorePhone.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/recyclerviewapplication/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -29,6 +32,49 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        ImageView icMenu = findViewById(R.id.ic_menu);
+        icMenu.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+            try {
+                java.lang.reflect.Field[] fields = popupMenu.getClass().getDeclaredFields();
+                for (java.lang.reflect.Field field : fields) {
+                    if ("mPopup".equals(field.getName())) {
+                        field.setAccessible(true);
+                        Object menuPopupHelper = field.get(popupMenu);
+                        Class<?> classPopupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                        java.lang.reflect.Method setForceShowIcon =
+                                classPopupHelper.getMethod("setForceShowIcon", boolean.class);
+                        setForceShowIcon.invoke(menuPopupHelper, true);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            popupMenu.setOnMenuItemClickListener(item -> {
+                int id = item.getItemId();
+                if (id == R.id.menu_search) {
+                    Toast.makeText(MainActivity.this, "Search clicked", Toast.LENGTH_SHORT).show();
+                    // startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                    return true;
+                } else if (id == R.id.menu_cart) {
+                    Toast.makeText(MainActivity.this, "Cart clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.menu_profile) {
+                    Toast.makeText(MainActivity.this, "Profile clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.menu_settings) {
+                    Toast.makeText(MainActivity.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            });
+
+            popupMenu.show();
+        });
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
@@ -53,8 +99,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_search) {
+            Toast.makeText(this, "Toolbar Search clicked", Toast.LENGTH_SHORT).show();
             return true;
         } else if (id == R.id.action_cart) {
+            Toast.makeText(this, "Toolbar Cart clicked", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
